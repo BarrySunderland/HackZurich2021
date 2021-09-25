5a8237cea1b1c27551959481c08778a791d67998
adahiya <a.dahiya@visium.ch>
2021-09-26 00:43:13 +0200

    path corrected for location_df

--- data_processing/src/data_processor.py
+++ data_processing/src/data_processor.py
@@ -3,14 +3,14 @@ import tqdm
 from src.paths import PATHS
 import os
 
+
 class DataProcessor:
-    
     @staticmethod
     def gen_proc_file_name(file_name):
         return os.path.join(PATHS.data, "processed", file_name)
 
     @staticmethod
-    def add_rssi_fields_to_disruptions(save:bool=True):
+    def add_rssi_fields_to_disruptions(save: bool = True):
         """This methods perform inner join on disruptions with the rssi values.
         This results in obtaining values in rssi table for events of interest.
 
@@ -37,9 +37,9 @@ class DataProcessor:
         if save:
             result_df.to_csv(DataProcessor.gen_proc_file_name("disruption.csv"))
         return pd.concat(merged_dfs)
-    
+
     @staticmethod
-    def add_rssi_fields_to_events(save:bool=True):
+    def add_rssi_fields_to_events(save: bool = True):
         """This methods perform inner join on disruptions with the rssi values.
         This results in obtaining values in rssi table for events of interest.
 
@@ -67,14 +67,24 @@ class DataProcessor:
         if save:
             result_df.to_csv(DataProcessor.gen_proc_file_name("events.csv"))
         return pd.concat(merged_dfs)
-    
+
     @staticmethod
-    def combine_events(save:bool=True):
+    def combine_events(save: bool = True):
         """This methods combines "A1_TotalTel","A1_ValidTel","A2_RSSI","A2_TotalTel","A2_ValidTel"
         for a day and location based on day and distance.
         """
         # RSSI data is too big to load onn cpu at a time.
-        column_of_interest = ["PositionNoLeap","Date", "Latitude","Longitude","A1_TotalTel","A1_ValidTel","A2_RSSI","A2_TotalTel","A2_ValidTel"]
+        column_of_interest = [
+            "PositionNoLeap",
+            "Date",
+            "Latitude",
+            "Longitude",
+            "A1_TotalTel",
+            "A1_ValidTel",
+            "A2_RSSI",
+            "A2_TotalTel",
+            "A2_ValidTel",
+        ]
         rssi_df_s = pd.read_csv(
             PATHS.rssi,
             chunksize=10000,
@@ -85,17 +95,21 @@ class DataProcessor:
         merged_dfs = []
         with tqdm.tqdm(total=3098) as pbar:
             for rssi_df in rssi_df_s:
-                rssi_df["Date"] = pd.to_datetime(rssi_df['DateTime']).dt.date
-                merged_dfs.append(rssi_df[column_of_interest].groupby(by=["Date", "PositionNoLeap"]).mean())
+                rssi_df["Date"] = pd.to_datetime(rssi_df["DateTime"]).dt.date
+                merged_dfs.append(
+                    rssi_df[column_of_interest]
+                    .groupby(by=["Date", "PositionNoLeap"])
+                    .mean()
+                )
                 pbar.update(1)
         result_df = pd.concat(merged_dfs)
         if save:
-            result_df.to_csv(DataProcessor.gen_proc_file_name(PATHS.rssi))
+            result_df.to_csv(DataProcessor.gen_proc_file_name("rssi.csv"))
         return pd.concat(merged_dfs)
-    
+
     @staticmethod
     def make_positional_mapping(save=True):
-        column_of_interest = ["PositionNoLeap", "Latitude","Longitude"]
+        column_of_interest = ["PositionNoLeap", "Latitude", "Longitude"]
         rssi_df_s = pd.read_csv(
             PATHS.rssi,
             chunksize=10000,
@@ -106,23 +120,32 @@ class DataProcessor:
         merged_dfs = []
         with tqdm.tqdm(total=3098) as pbar:
             for rssi_df in rssi_df_s:
-                temp =rssi_df[column_of_interest].groupby(by=["PositionNoLeap"]).mean().reset_index()
-                temp["Position_m"] = temp["PositionNoLeap"]//1000
+                temp = (
+                    rssi_df[column_of_interest]
+                    .groupby(by=["PositionNoLeap"])
+                    .mean()
+                    .reset_index()
+                )
+                temp["Position_m"] = temp["PositionNoLeap"] // 1000
                 temp = temp.groupby(by=["Position_m"]).mean()
                 merged_dfs.append(temp)
                 pbar.update(1)
-    #             break
+        #             break
         result_df = pd.concat(merged_dfs)
-        result_df =  result_df.reset_index().drop_duplicates('Position_m').set_index('Position_m')
+        result_df = (
+            result_df.reset_index()
+            .drop_duplicates("Position_m")
+            .set_index("Position_m")
+        )
         if save:
-            result_df.to_csv(PATHS.data+"/location.csv")
+            result_df.to_csv(DataProcessor.gen_proc_file_name("location.csv"))
         return result_df
 
 
 def main():
     print("1. Adding RSSI values to disruption")
     disruption_path = DataProcessor.gen_proc_file_name("disruption.csv")
-    location_path =  DataProcessor.gen_proc_file_name("location.csv")
+    location_path = DataProcessor.gen_proc_file_name("location.csv")
     if not os.path.exists(disruption_path):
         DataProcessor.add_rssi_fields_to_disruptions()
     else:
@@ -135,5 +158,5 @@ def main():
         print("skipping location processing, file already exists!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
