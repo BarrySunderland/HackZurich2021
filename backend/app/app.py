a42ab47c9faec97374c736932f4480d2255623db
Claudio Vigliarolo <claudio.vigliarolo@kerr-italy.it>
2021-09-25 22:15:37 +0200

    temp fix

--- backend/app/app.py
+++ backend/app/app.py
@@ -6,128 +6,127 @@ import json
 from make_plot import make_sensor_plot, make_dummy_plot
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-
-DEBUG=True
+DEBUG = True
 
 SHOW_FAILURES = True
 SHOW_HORIZON = True
-HOST="localhost"
-PORT="5000"
-PROTOCOL="http"
+HOST = "localhost"
+PORT = "5000"
+PROTOCOL = "http"
 MODEL_CSV = "./data/aneesh.csv"
 
 # quick fix
 # Must be a library for handling this
 ROUTES = [	"/",
-			"/api",
-			"/api/failures",
-			"/plot/sensor",
-			"/plot/dummy"
-			"/api/horizon"
-			]
+           "/api",
+           "/api/failures",
+           "/plot/sensor",
+           "/plot/dummy",
+           "/api/horizon"
+           ]
 
-ENDPOINTS = [f"{PROTOCOL}://{HOST}:{PORT}{route}" for route in ROUTES] 
+ENDPOINTS = [f"{PROTOCOL}://{HOST}:{PORT}{route}" for route in ROUTES]
 if DEBUG:
-	print(ENDPOINTS)
+    print(ENDPOINTS)
 
 
 def convert():
-	convert_csv_to_geojson(MODEL_CSV)
+    convert_csv_to_geojson(MODEL_CSV)
 
 
 def get_horizon():
-	"""return a list of predicted failure points in the next seven days"""
-	if SHOW_HORIZON:
-		convert()
-		horizon_geojson = "./data/horizon.geojson"
+    """return a list of predicted failure points in the next seven days"""
+    if SHOW_HORIZON:
+        convert()
+        horizon_geojson = "./data/horizon.geojson"
 
-		with open(horizon_geojson, "r") as f:
-			horizon_geojson = json.load(f)
-		return horizon_geojson
-	else:
-		return []
-		
+        with open(horizon_geojson, "r") as f:
+            horizon_geojson = json.load(f)
+        return horizon_geojson
+    else:
+        return []
+
 
 def get_failures():
-	"""return a list dictionaries with failure details"""	
-	if SHOW_FAILURES:
-		fpath = "./data/sample_frontend.json"
-		with open(fpath,"r") as f:
-			failures_json = json.load(f)
+    """return a list dictionaries with failure details"""
+    if SHOW_FAILURES:
+        fpath = "./data/sample_frontend.json"
+        with open(fpath, "r") as f:
+            failures_json = json.load(f)
 
-		# failures = []
-		return failures_json
-	else:
-		return []
+        # failures = []
+        return failures_json
+    else:
+        return []
 
 
 def render_endpoint_list():
 
-	routes_el_list = ""
-	
-	for endpoint, route in zip(ENDPOINTS,ROUTES):
-		routes_el_list += f"<li><a href={endpoint}>{route}</a></li>"
-	
-	return f"""<h4>Enpoints</h4>
+    routes_el_list = ""
+
+    for endpoint, route in zip(ENDPOINTS, ROUTES):
+        routes_el_list += f"<li><a href={endpoint}>{route}</a></li>"
+
+    return f"""<h4>Enpoints</h4>
 				<ul>{routes_el_list}</ul>
 			"""
 
 
 @app.route('/')
 def hello():
-	
 
-	return """
+    return """
 	<h1>Siemens  - Track Bugscreen</h1>
 	<img src="http://hackzurich.siemens.cool/2021/2021-website/img/LeimbachZetzwilMuffe_delay120_loop0.c037c005.gif" height=400></img>
 	
 	""" + render_endpoint_list()
 
 
-
 @app.route('/api')
 def api_root():
-	
-	return render_endpoint_list()
+
+    return render_endpoint_list()
+
 
 @app.route('/api/horizon')
 def horizon_summary():
-	horizon_summary = get_horizon()
+    horizon_summary = get_horizon()
 
-	if horizon_summary:
-		return jsonify(results=horizon_summary)
-	else:
-		return {"msg":"oracle not available"}
+    if horizon_summary:
+        return jsonify(results=horizon_summary)
+    else:
+        return {"msg": "oracle not available"}
 
 
 @app.route('/api/failures')
 def failure_summary():
-	
-	failures_json = get_failures()
 
-	if failures_json:
-		return jsonify(results=failures_json)
-	else:
-		return {"msg":"no failures"}
+    failures_json = get_failures()
+
+    if failures_json:
+        return jsonify(results=failures_json)
+    else:
+        return {"msg": "no failures"}
+
 
 @app.route('/plot/sensor')
 def plot_sensor():
 
-	plot_json = make_sensor_plot()
+    plot_json = make_sensor_plot()
 
-	return plot_json
+    return plot_json
 
 
 @app.route('/plot/dummy')
 def plot_dummy():
 
-	plot_json = make_dummy_plot()
+    plot_json = make_dummy_plot()
 
-	return plot_json
+    return plot_json
 
 
 if __name__ == '__main__':
-	app.run(host=HOST,port=PORT, debug=DEBUG)
+    app.run(host=HOST, port=PORT, debug=DEBUG)
